d3e5b73d43cb22b4ee94ac0976e67501509e5ca4
kris-t-tsoi <kris-t-tsoi@gmail.com>
2016-08-05 15:26:55 +1200

    create empty methods

--- process_message_system.py
+++ process_message_system.py
@@ -3,8 +3,40 @@ import os
 
 class MessageProc():
 
-	#set up communication mechanism
+	#set up communication mechanism (named pipes)
 	def main ():
+
+
+	
+
+
+
+
+	#start up a new process and return process id to parent process
+	def start():
+
+
+
+
+	#fork
+
+
+	#child calls main()
+
+
+
+
+
+
+
+
+	#send the input parameter message items it receives to the recieve()
+	def give():
+
+
+
+
+	def recieve():
 
 
 
