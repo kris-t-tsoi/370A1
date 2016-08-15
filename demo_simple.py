0512b80e7c576fc97e63ac8474f93d300c5ad6c4
kris-t-tsoi <kris.t.tsoi@gmail.com>
2016-08-15 15:41:26 +1200

    change how tup is appended

---
+++ demo_simple.py
@@ -0,0 +1,30 @@
+from process_message_system import *
+import sys
+
+class Consumer(MessageProc):
+
+    def main(self):
+        super().main()
+        
+        n=0
+        
+        while True:
+            self.receive(
+                Message(
+                    'data',
+                    action=lambda x: print(x)),
+                Message(
+                    'stop',
+                    action=lambda: sys.exit()))
+            n=n+1
+            if n==1000:
+                print("it was stuck")
+                break;
+
+if __name__=='__main__': # really do need this
+    me = MessageProc()
+    me.main()
+    consumer = Consumer().start()
+    for num in range(1000):
+        me.give(consumer, 'data', num + 1)
+    me.give(consumer, 'stop')
---
+++ process_message_system.py
@@ -0,0 +1,120 @@
+import sys
+import os
+import pickle
+
+import time
+
+
+class MessageProc():
+
+	#set up communication mechanism (named pipes)
+	def main (self):
+		print("main method in parent")
+		
+		#create named pipe
+		os.mkfifo('/tmp/pipe'+str(os.getpid()))
+
+	
+
+
+
+
+	#start up a new process and return process id to parent process
+	def start(self):		
+
+		#fork
+		pid = os.fork()
+		
+		#If child fork
+		if pid == 0:
+			#go into the main()
+			self.main()
+			
+		#if parent fork
+		else:
+			#return pid of the child fork
+			return pid
+
+
+
+	#send the input parameter message items it receives to the recieve()
+	#pid - pid of child fork 
+	#messageID - id type of message sent (what is checked in recieve)
+	#values - not nessary to pass in
+	def give(self, pid, messageID, *values):
+		
+		pipe = '/tmp/pipe'+str(pid)
+		
+		#check communitcation process is up else sleep for a bit
+		if not os.path.exists(pipe):
+			time.sleep(0.5)
+		
+		fifo = open(pipe,'wb')
+		
+		#parent fork gives the message
+		
+		tup = []
+		tup.append(pid)
+		tup.append(messageID)
+		tup.append(values)
+		pickle.dump(tup,fifo)
+		
+		#print('in give()')
+		#print('messageID is '+str(messageID))
+		
+		pass
+
+
+
+#check out os atExit and clean up named pipes
+
+	#check message does not exist in queue and remove executed messages
+	def receive(self, *messages):
+
+		pipe = '/tmp/pipe' + str(os.getpid())
+
+		# check communitcation process is up else sleep for a bit
+		if not os.path.exists(pipe):
+			time.sleep(0.5)
+
+		fifo = open(pipe, 'r')
+
+
+		#for line in
+		
+		
+		#child fork recieves the message
+		
+		for mess in messages:
+			
+			#if message ID is ANY then execute first item in give queue
+			if mess.messageID == 'ANY':
+				pass
+			
+			else:
+				pass
+			
+			
+			#put time out here
+			
+		pass
+		
+	#what to do when system ends
+	def removeGarbage(self):
+		pass
+		#remove all pipes
+	
+	#import atexit
+	#atexit.register(removeGarbage())
+
+class Message():
+	
+	def __init__(self,messageID, action):
+		
+		self.messageID = messageID
+		self.action=action
+		
+		#pass
+		
+class TimeOut():
+	pass
